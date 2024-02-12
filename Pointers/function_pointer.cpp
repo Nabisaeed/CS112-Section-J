03d1172cd1e650896dc520b037f8edcbaecb4965
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2024-02-12 19:11:28 +0500

    Update function_pointer.cpp

--- Pointers/function_pointer.cpp
+++ Pointers/function_pointer.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 // Function prototype for addition
+typedef int (*pointer_function)(int, int);
 int add(int, int);
 
 // Function to perform calculations using function pointers
-void calculator(int a, int b, int (*p1)(int, int)) {
-    cout << "The addition of two numbers is : " << p1(a, b) << endl;
+void calculator(int a, int b, pointer_function c1) {
+    cout << "The addition of two numbers is : " << c1(a, b) << endl;
 }
 
 int main (){
+
+    pointer_function p1;
     
-    int (*p1)(int, int);  // Declaring pointer for storing function address 
+      // Declaring pointer for storing function address 
     p1=&add;     // assigning address of function add
     
     calculator(20, 50, p1); // calling function calculator 
