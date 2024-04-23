9c14688278e6d365b6b1717c0d54cba5df1c37c6
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2024-04-23 14:58:31 +0500

    31th Lecture - Namespaces and Typecasting

---
+++ 31th Lecture/Typecasting.cpp
@@ -0,0 +1,18 @@
+// C++ Program starts here
+#include <iostream>
+using namespace std;
+    
+    
+int main()
+{
+    
+    int a=100, b=33;
+    
+    float c;
+    
+    c=float(a/b);
+   
+    cout<<(c)<<endl;
+     return 0;
+    
+}
---
+++ 31th Lecture/namespaces11.cpp
@@ -0,0 +1,21 @@
+// C++ Program starts here
+
+#include <iostream>
+using namespace std;
+
+// Namespace declaration
+namespace name1 {
+    // Declaration of an integer variable named 'cout' inside namespace 'name1'
+    int cout = 1000;
+}
+
+using namespace name1;
+int main() {
+    // Declaration of an integer variable named 'cout' in the main function scope
+    int cout = 100;
+    
+    // Printing the value of the local variable 'cout' followed by the value of 'name1::cout'
+    std::cout << cout << name1::cout;
+    
+    return 0;
+}
